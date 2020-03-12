611b68edf45796b842bc9237213a0411c5212d36
Thomas M Steenholdt <tmus@tmus.dk>
2020-03-12 10:43:45 -0300

    updates to better fit personal needs

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
 # checkmk-notify-teams
 
-if you (unfortunately) find yourself utilizing Microsoft Teams incoming webhooks & need to send alerts from CheckMK, you can use this. utilizes params in UI to set the webhook url so you can send to different channels / don't hardcore webhook
+If you are using CheckMK and Microsoft Teams, this plugin can be used to send notifications to a teams webhook.
+
+The plugin utilizes params in UI to set the webhook url.
 
 ![Example Alert](img/example-alert.png)
 
 ## setup
 
-- clone / copy teams.py & put in `/omd/sites/{your_site}/local/share/check_mk/notifications`
-- ensure it's executable (`chmod +x teams.py`)
-- setup notification. method = 'Notify via Microsoft Teams', param = teams channel webhook
+- Install teams.py as `/omd/sites/{your_site}/local/share/check_mk/notifications/teams`
+- ensure it's executable (`chmod +x teams`)
+- setup notification. method = 'Microsoft Teams', param = webhook url
 ![Example Config](img/example-config.png)
 
 ## Credits
 
-checkmk team -- this is a modified version of their slack webhook plugin
+CheckMK Team -- this is a modified version of their slack webhook plugin
+RileyMichael -- this plugin is based on his work (https://github.com/RileyMichael)
--- teams.py
+++ teams.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
-# Notify via Micrsoft Teams
+# -*- coding: utf-8 -*-
+# Microsoft Teams
+#
+r"""
+Send notification messages to Teams
+===================================
 
+Use a teams webhook to send notification messages
+"""
 import os
 import requests
 import sys
@@ -15,47 +22,49 @@ COLORS = {
     "UNREACHABLE": "#CCCCCC",
 }
 
+
 def teams_msg(context):
     """Build the message for teams"""
     facts = []
-    if context.get('WHAT', None) == "SERVICE":
-        state = context["SERVICESTATE"]
-        color = COLORS.get(state)
-        subtitle = "Service Notification"
-        facts.append({"name": "Service:", "value": context["SERVICEDESC"]})
-        output = context["SERVICEOUTPUT"] if context["SERVICEOUTPUT"] else ""
-    else:
-        state = context["HOSTSTATE"]
-        color = COLORS.get(state)
-        subtitle = "Host Notification"
-        output = context["HOSTOUTPUT"] if context["HOSTOUTPUT"] else ""
 
-    facts.extend([
-        {
-            "name": "Host:",
-            "value": context["HOSTNAME"]
-        },
-        {
-            "name": "State:",
-            "value": state
-        }
-    ])
+    # Service notification
+    if context.get('WHAT', None) == "SERVICE":
+        color = COLORS.get(context["SERVICESTATE"])
+        title = "Service {NOTIFICATIONTYPE} notification".format(**context)
+        facts.extend([
+            {"name": "Service:", "value": context["SERVICEDESC"]},
+            {"name": "Service state:", "value": context["SERVICESTATE"]},
+            {"name": "Host:", "value": context["HOSTNAME"]}
+        ])
+        output = context["SERVICEOUTPUT"] if context["SERVICEOUTPUT"] else ""
+
+    # Host notification
+    else:
+        color = COLORS.get(context["HOSTSTATE"])
+        title = "Host {NOTIFICATIONTYPE} notification".format(**context)
+        facts.extend([
+            {"name": "Host:", "value": context["HOSTNAME"]},
+            {"name": "Host state:", "value": context["HOSTSTATE"]}
+        ])
+        output = context["HOSTOUTPUT"] if context["HOSTOUTPUT"] else ""
 
     return {
         "@type": "MessageCard",
         "@context": "https://schema.org/extensions",
-        "summary": subtitle,
+        "summary": title,
         "themeColor": color,
         "sections": [
             {
-                "activityTitle": "CheckMK",
-                "activitySubtitle": subtitle,
-                "activityImage": "https://checkmk.com/images/favicon.png",
+                # "activityTitle": "CheckMK",
+                "activityTitle": title,
+                # "activityImage": "https://checkmk.com/images/favicon.png",
+                # "activitySubtitle": title,
                 "facts": facts,
                 "text": output
             }
         ]
     }
+
 
 def collect_context():
     return {
@@ -63,6 +72,7 @@ def collect_context():
         for (var, value) in os.environ.items()
         if var.startswith("NOTIFY_")
     }
+
 
 def post_request(message_constructor, success_code=200):
     context = collect_context()
@@ -76,6 +86,7 @@ def post_request(message_constructor, success_code=200):
         sys.stderr.write(
             "Failed to send notification. Status: %i, Response: %s\n" % (r.status_code, r.text))
         sys.exit(2)
+
 
 if __name__ == "__main__":
     post_request(teams_msg)
